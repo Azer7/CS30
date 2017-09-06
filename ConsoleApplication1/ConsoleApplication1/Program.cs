c853309d1cca89d77aab96fea82e71c34c3e6b87
Azer7 <30532122+Azer7@users.noreply.github.com>
2017-09-06 10:44:37 -0600

    Math parser

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            while (input == "")
+                input = Console.ReadLine();
+
+
+            Parser.ParseExpression(input);
+            Console.ReadLine();
+        }
+    }
+
+    public class Parser
+    {
+        public static void ParseExpression(string expression)
+        {
+            Console.WriteLine("Input: " + expression);            
+
+            Regex
+
+            if (expression.Except("1234567890+-*/^").Any())
+                Console.WriteLine("error");
+            else
+            { //parser program
+                List<int> valueArray = new List<int>();
+    
+                
+                
+
+
+            }
+        }
+    }
+}
