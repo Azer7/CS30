66ad78cf8bf8d041fe35d39d5654e865859df860
Azer7 <30532122+Azer7@users.noreply.github.com>
2017-09-27 00:30:25 -0600

    fixed binary search

--- Iterative Algorithms/Spell Checker Start/script.js
+++ Iterative Algorithms/Spell Checker Start/script.js
@@ -16,17 +16,21 @@ $("#check-word-bin").click(function () {
 
 function checkWord(search) {
     // Get the word the user typed into the input element
-    var word = $("#word").val();
+    let word = $("#word").val();
+    let indexResult;
+
     let time = Date.now();
     if (search == "linear")
-        linearWordSearch(dictionary, word);
+        result = linearWordSearch(dictionary, word);
     else
-        binaryWordSearch(dictionary, word);
+        result = binaryWordSearch(dictionary, word);
     time = Date.now() - time;
 
     // Check and report if the word is in the dictionary
-    $("#result").html(word + " is in the dictionary. " + time);
-
+    if (result >= 0)
+        $("#result").html(time + "ms: <br/>" + word + " is in the dictionary at index: " + result);
+    else
+        $("#result").html("not in dictionary");
 }
 
 // Search
@@ -41,13 +45,13 @@ function linearWordSearch(arr, word) {
 function binaryWordSearch(arr, word) {
     let lowerIndex = 0;
     let upperIndex = arr.length - 1;
-    let middleIndex = Math.floor((arr.length - 1) / 2);
+
     while (lowerIndex <= upperIndex) { //review can I go <?
+        let middleIndex = Math.floor((upperIndex + lowerIndex) / 2);
         if (arr[middleIndex] == word)
             return middleIndex;
-        else if (true) {
-
-        }
+        else
+            word > arr[middleIndex] ? lowerIndex = middleIndex + 1 : upperIndex = middleIndex - 1;
     }
     return -1;
 }
