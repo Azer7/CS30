824277156da99f1995a37b5f1d1a05cb7814c2de
Azer7 <30532122+Azer7@users.noreply.github.com>
2017-10-24 23:07:34 -0600

    ridic

--- Ray Casting (P5JS)/Ray.js
+++ Ray Casting (P5JS)/Ray.js
@@ -2,9 +2,9 @@ class Ray {
     constructor(x, y, angle) {
         this.pos = createVector(0, 0);
         this.posEnd = createVector(0, 0);
-        this.angle;
+        this._angle;
         this.maxLength = 100;
-        this.length = this.maxLength; //will be changed if collision is found
+        this._length = this.maxLength; //will be changed if collision is found
         //ray slope intercept form
         this.slope;
         this.yIntercept;
@@ -20,25 +20,37 @@ class Ray {
             this.slope = Math.tan(this.angle);
         }
 
-        this.posEnd.x;
+        this.evaluateEndpoint();
         //y = slope*x + yIntercept
         //yIntercept = y - slope*x
         this.yIntercept = this.pos.y - this.slope * this.pos.x;
     }
 
+    get angle() {
+        return this._angle;
+    }
     set angle(degrees) {
         //change to radians
+        //360 - degrees to switch it from clockwise to counter clockwise
+        this._angle = ((360 - degrees) * Math.PI / 180);
     }
 
+    get length() {
+        return this._length;
+    }
     set length(newLen) {
-        console.log("length set");
-        if (newLen != this.length) {
-        // get new end point
-            this.length = newLen;
-            this.endPos.x = this.pos.x + Math.cos(angle) * this.length
-            
-
+        if (newLen != this._length) {
+            // get new end point
+            this._length = newLen;
         }
+    }
+
+    evaluateEndpoint() {
+        //Math.cos(angle) + this.pos.x / this._length = this.endPos.x / this._length
+        //this.endPos.x = this.pos.x + Math.cos(angle) * this._length; //
+        this.posEnd.x = this._length * (Math.cos(this.angle) + this.pos.x / this._length);
+        //this.endPos.y = this.pos.y + Math.sin(angle) * this._length;
+        this.posEnd.y = this._length * (Math.sin(this.angle) + this.pos.y / this._length);
     }
 
     checkCollisions(objects) {
@@ -54,12 +66,30 @@ class Ray {
                 }
             }
         }
-        
+
         this.length = shortest;
     }
 
     getCollisionPoint(line) {
+        let collision;
+        let hasCollided = false;
 
+        if (this.slope - line.slope == 0)
+            if (this.yIntercept - line.yIntercept == 0)
+                collision = createVector(this.x, this.y); //exact same line~
+            else
+                return;
+        else { //not blatantly undefined
+            
+        }
+        return collision;
+    }
+
+    draw() {
+        stroke("black");
+        strokeWeight(2);
+
+        line(this.pos.x, this.pos.y, this.posEnd.x, this.posEnd.y);
     }
 }
 
--- Ray Casting (P5JS)/main.js
+++ Ray Casting (P5JS)/main.js
@@ -5,9 +5,9 @@ let upRay;
 function setup() {
     createCanvas(800, 600);
     
-    objects.push(new Border(20, 10, width - 20, 70)); //line across screen
+    objects.push(new Border(20, 10, width - 20, 90)); //line across screen
     
-    upRay = new Ray(width / 2, height / 2, 30); //x, y, angle
+    upRay = new Ray(width / 2, height / 2, 270); //x, y, angle
 }
 
 function draw() {
@@ -19,5 +19,7 @@ function draw() {
     }
 
     //check for intersections
-    upRay.checkCollisions(objects);
+    //upRay.checkCollisions(objects);
+    
+    upRay.draw();
 }
