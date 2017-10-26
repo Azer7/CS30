4cd9273e05f190892563b7fe434dbf17d922ba68
Azer7 <30532122+Azer7@users.noreply.github.com>
2017-10-25 23:03:32 -0600

    eyo
    bug is that the line extends, even though it is only partially drawn

--- Ray Casting (P5JS)/main.js
+++ Ray Casting (P5JS)/main.js
@@ -9,15 +9,11 @@ let ray;
 function setup() {
     createCanvas(800, 600);
 
-    objects.push(new Border(0, 10, width, 120)); //line across screen
-    objects.push(new Border(0, height - 120, width, height - 10)); //line across screen
+    objects.push(new Border(0, 0, width, 150)); //line across screen
 
-    
-    ray = new Ray(width / 2, height / 2, 0); //x, y, angle
 
-    for (let i = 0; i < 400; i++) {
-        rays.push(new Ray(width / 2, height / 2, i * (360 / 1000)));
-    }
+    ray = new Ray(width / 2, height / 2, 270); //x, y, angle
+    //rays.push(new Ray(width / 2, height / 2, i * (360 / 1000)));
 }
 
 function draw() {
@@ -28,25 +24,25 @@ function draw() {
         objects[i].draw();
     }
 
-    for (let i = 0; i < rays.length; i++) {
-        if (keyIsDown(RIGHT_ARROW)) {
-            rays[i].angle -= 1;
-        } else if (keyIsDown(LEFT_ARROW)) {
-            rays[i].angle += 1;
-        }
+    //    for (let i = 0; i < rays.length; i++) {
+    //        if (keyIsDown(RIGHT_ARROW)) {
+    //            rays[i].angle -= 1;
+    //        } else if (keyIsDown(LEFT_ARROW)) {
+    //            rays[i].angle += 1;
+    //        }
+    //
+    //        //check for intersections
+    //        rays[i].checkCollisions(objects);
+    //
+    //        rays[i].draw();
+    //    }
 
-        //check for intersections
-        rays[i].checkCollisions(objects);
-
-        rays[i].draw();
+    if (keyIsDown(RIGHT_ARROW)) {
+        ray.angle -= 1;
+    } else if (keyIsDown(LEFT_ARROW)) {
+        ray.angle += 1;
     }
 
-//    if (keyIsDown(RIGHT_ARROW)) {
-//        ray.angle -= 1;
-//    } else if (keyIsDown(LEFT_ARROW)) {
-//        ray.angle += 1;
-//    }
-//
-//    ray.checkCollisions(objects);
-//    ray.draw();
+    ray.checkCollisions(objects);
+    ray.draw();
 }
