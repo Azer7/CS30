93c38151d0d9b349641a456c04bd94ab7fb670ad
Azer7 <30532122+Azer7@users.noreply.github.com>
2017-12-05 10:45:34 -0700

    asdfasdf

--- Second Langue Programming (C++)/Classes, Objects/Classes, Objects/Classes, Objects.cpp
+++ Second Langue Programming (C++)/Classes, Objects/Classes, Objects/Classes, Objects.cpp
@@ -15,7 +15,7 @@ public:
 	vector<string> names;
 	int age;
 	void setName(std::string);
-	void getInitials();
+	string getInitials();
 
 private:
 };
@@ -28,20 +28,55 @@ Person::Person()
 void Person::setName(string fullName) {
 	int space = 0;
 
-	for (int i = 0; i < fullName.length(); i++) {
+	for (int i = 0; i < fullName.length(); i++) { //loops through the fullName and cuts out the name at every space
 		if (fullName[i] == ' ') {
-			names.push_back(fullName.substr(space, i));
-			space = i;
+			names.push_back(fullName.substr(space, i - space));
+			space = i + 1;
 		}
 	}
+	names.push_back(fullName.substr(space));
+}
+
+
+string Person::getInitials() {
+	string initials;
+	initials.push_back(names[0][0]); //pushes back a character
+									 //you cannot use initials += names[0][0] as names[0][0] is a character, not a string
+	initials += ". "; //this is a string however
+	initials.push_back(names[names.size() - 1][0]); //not this
+	initials += "."; //this!
+	return(initials); //returns the formed string
+}
+
+
+class Family
+{
+public:
+	Family();
+
+private:
+
+};
+
+Family::Family()
+{
+}
+
+Family::~Family()
+{
 }
 
 int main()
 {
 	Person person;
-	person.setName("Billy Bob Forest");
+	string inputName = "";
+	while (inputName == "") { //waits for the user to actually enter something
+		getline(cin, inputName); //gets the entire line
+	}
+	person.setName(inputName); //calls the setName function, with input as (inputName)
+
+	person.getInitials(); // calls the object method to get initials which returns a str
 	int delay;
 	cin >> delay;
-
-    return 0;
+	return 0;
 }
