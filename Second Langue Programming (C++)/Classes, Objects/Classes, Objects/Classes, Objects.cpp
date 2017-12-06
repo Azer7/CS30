333e08c34dc4f123213d0c5056597ede7acc0a9a
Azer7 <30532122+Azer7@users.noreply.github.com>
2017-12-06 10:45:19 -0700

    yee

--- Second Langue Programming (C++)/Classes, Objects/Classes, Objects/Classes, Objects.cpp
+++ Second Langue Programming (C++)/Classes, Objects/Classes, Objects/Classes, Objects.cpp
@@ -15,7 +15,7 @@ public:
 	vector<string> names;
 	int age;
 	void setName(std::string);
-	string getInitials();
+	string getName();
 
 private:
 };
@@ -34,17 +34,19 @@ void Person::setName(string fullName) {
 			space = i + 1;
 		}
 	}
-	names.push_back(fullName.substr(space));
+
+	names.push_back(fullName.substr(space)); //adds the last word too
 }
 
 
-string Person::getInitials() {
-	string initials;
-	initials.push_back(names[0][0]); //pushes back a character
-									 //you cannot use initials += names[0][0] as names[0][0] is a character, not a string
-	initials += ". "; //this is a string however
-	initials.push_back(names[names.size() - 1][0]); //not this
-	initials += "."; //this!
+string Person::getName() {
+	string initials; //creates a string to add to
+	if (names.size() > 1) {
+		initials.push_back(names[0][0]); //pushes back a character
+										 //you cannot use initials += names[0][0] as names[0][0] is a character, not a string
+		initials += ". "; //this is a string however
+	}
+	initials += names[names.size() - 1];
 	return(initials); //returns the formed string
 }
 
@@ -52,31 +54,50 @@ string Person::getInitials() {
 class Family
 {
 public:
-	Family();
+	vector<Person> members;
+	int getTotalAge(); //declares a function
 
 private:
 
 };
 
-Family::Family()
-{
-}
-
-Family::~Family()
-{
+int Family::getTotalAge() { //code of function
+	int total = 0;
+	for (int i = 0; i < members.size(); i++) {
+		total += members[i].age;
+	}
+	return total;
 }
 
 int main()
 {
-	Person person;
-	string inputName = "";
-	while (inputName == "") { //waits for the user to actually enter something
-		getline(cin, inputName); //gets the entire line
-	}
-	person.setName(inputName); //calls the setName function, with input as (inputName)
+	string inputName; //initialized to "" automatically
+	int inputAge = -1; //initializes to -1, as noone can have a (-) age
+	Family family; //creates a new family class
+	cout << "Create a Family!" << endl;
 
-	person.getInitials(); // calls the object method to get initials which returns a str
-	int delay;
-	cin >> delay;
+	do {
+		inputName = "";
+		inputAge = -1;
+		Person person;
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "What is you name my dude?" << endl;
+		getline(cin, inputName); //gets the entire line	
+		person.setName(inputName); //calls the setName function, with input as (inputName)
+
+		cout << "How about your age?" << endl;
+		cin >> inputAge;
+		person.age = inputAge;
+
+		cout << "Hi " << person.getName() << endl; // calls the object method to get initials which returns a str
+
+		if (inputName != "" && inputAge != -1) {
+			family.members.push_back(person);
+		}
+	} while (inputName != "" || inputAge != -1);  //waits for the user to actually enter something
+	//needs to be declared earlier, so that line 95 is defined
+
+	cout << "Quite the family with a total age of " << family.getTotalAge() << "!";
+
 	return 0;
 }
